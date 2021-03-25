05ed2d2d5610a3e751274ff1de49f0ec6c10b734
lightmanLP <liteman1000@gmail.com>
2021-03-26 03:22:29 +0700

    circle fix

--- pybug_ascii_logo/main.py
+++ pybug_ascii_logo/main.py
@@ -1,5 +1,4 @@
-from typing import Set, Tuple, List, Optional
-import itertools
+from typing import Set, Tuple, Optional
 import math
 
 from colorama import Fore, Style
@@ -11,25 +10,29 @@ canvas = canvas.Canvas()
 
 def gen_circle_points(
     radius: int,
-    n: Optional[int] = 10000
+    n: Optional[int] = 1000,
+    terminal_resize: Optional[float] = 1
 ) -> Set[Tuple[int, int]]:
+    """ VS Code resize: 5 / 12 """
+
     result = set()
 
     for x in range(0, n + 1):
         point = 2 * math.pi / n * x
         result.add(
             (
-                int(math.cos(point) * radius) + radius,
-                int(math.sin(point) * radius * (24 / 48)) + radius
+                round(math.cos(point) * radius) + radius,
+                round(math.sin(point) * radius * terminal_resize) + radius
             )
         )
 
     return result
 
 
-points = gen_circle_points(50)
+points = gen_circle_points(50, terminal_resize=8 / 16)
 print(points)
 for i in points:
-    canvas.add_item(item.Item(f"@", i))
+    canvas.add_item(item.Item("@", i))
 
 print(canvas.render())
+input()
